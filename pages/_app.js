0bac3f791ef0d167760a40159f9adbab443fe432
Tyler Lai <lai.t.tyler@gmail.com>
2021-06-05 19:18:15 -0500

    refactor: Update _App to include layout, navbar, and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import 'tailwindcss/tailwind.css'
+import { motion } from "framer-motion";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <>
+      <Layout>
+        <Navbar />
+        <motion.div
+          className="min-h-full mb-20 mt-20 font-body"
+          key={router.route}
+          animate="pageAnimate"
+          initial="pageInitial"
+          variants={{ pageInitial: { opacity: 0 }, pageAnimate: { opacity: 1 } }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+        <Footer />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
