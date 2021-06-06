4e42afc23371f9c6c38ac2dca304fdeceb16f679
Tyler Lai <lai.t.tyler@gmail.com>
2021-06-05 20:34:26 -0500

    refactor: Update project data use uuid and include new project

--- components/ProjectCard/data.js
+++ components/ProjectCard/data.js
@@ -1,22 +1,34 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const projectData = [
   {
-    id: 1,
-    title: "The Guardian",
-    description: "A React SPA that fetches articles from the Guardian API.",
-    github: "https://github.com/TylerTLai/guardian-app",
-    link: "https://tylertlai.github.io/guardian-app/#/",
-    technologies: ["react", "redux", "styled components", "Guardian API "],
+    id: uuidv4(),
+    title: "Personal Site",
+    description: "A NextJS and Tailwind CSS personal website.",
+    github: "https://github.com/TylerTLai/nextjs-website",
+    link: "https://www.tylerlai.com/",
+    technologies: ["nextJS", "tailwindCSS", "Framer Motion"],
   },
   {
-    id: 2,
+    id: uuidv4(),
+    title: "The Guardian",
+    description:
+      "A React single page application that fetches articles from the Guardian API.",
+    github: "https://github.com/TylerTLai/guardian-app",
+    link: "https://tylertlai.github.io/guardian-app/#/",
+    technologies: ["react", "redux", "styled components", "Guardian API"],
+  },
+  {
+    id: uuidv4(),
     title: "Film Findr v2",
-    description: "A React web app that allows users to search for movies.",
+    description:
+      "A React single page application that allows users to search for movies.",
     github: "https://github.com/TylerTLai/film-findr-2",
     link: "https://tylertlai.github.io/film-findr-2/#/",
     technologies: ["react", "redux", "styled components", "TMDB API"],
   },
   {
-    id: 3,
+    id: uuidv4(),
     title: "Travel Site",
     description:
       "TRVL is a React single page application that allows users to get travel deals.",
@@ -25,9 +37,10 @@ export const projectData = [
     technologies: ["react", "react hooks", "CSS"],
   },
   {
-    id: 4,
+    id: uuidv4(),
     title: "Film Findr v1",
-    description: "A React web app that allows users to search for movies.",
+    description:
+      "A React single page application that allows users to search for movies.",
     github: "https://github.com/TylerTLai/film-findr",
     link: "https://film-findr.netlify.app/",
     technologies: ["react", "CSS", "TMDB API"],
