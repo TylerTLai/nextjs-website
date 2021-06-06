0eee598e2b59715bd019abcfc353a57e5744018e
Tyler Lai <lai.t.tyler@gmail.com>
2021-06-05 19:16:30 -0500

    feat: Create footer

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="pb-6">
+      <p className="text-xs tracking-widest text-gray-400">
+        Built & Designed by Tyler Lai.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
