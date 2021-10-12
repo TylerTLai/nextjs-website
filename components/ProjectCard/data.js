880949bdc63ded80fb3c5d87dece13e8d89d529e
Tyler Lai <lai.t.tyler@gmail.com>
2021-10-12 01:10:05 -0500

    refactor: Remove old project

--- components/ProjectCard/data.js
+++ components/ProjectCard/data.js
@@ -45,13 +45,4 @@ export const projectData = [
     link: "https://tylertlai.github.io/react-travel-site/#/",
     technologies: ["react", "react hooks", "CSS"],
   },
-  {
-    id: uuidv4(),
-    title: "Film Findr v1",
-    description:
-      "A React single page application that allows users to search for movies.",
-    github: "https://github.com/TylerTLai/film-findr",
-    link: "https://film-findr.netlify.app/",
-    technologies: ["react", "CSS", "TMDB API"],
-  },
 ];
